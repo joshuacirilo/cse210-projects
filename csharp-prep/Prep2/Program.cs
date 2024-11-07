1b6c4c04faa6a7a77d112dc3981473cded084fdd
joshuacirilo <u175692795@byui.edu>
2024-11-07 16:25:47 -0600

    practice

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,51 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string percentage =  Console.ReadLine();
+        int percent = int.Parse(percentage);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >=70)
+        {
+            letter = "C";
+        }
+        else if(percent >=60)
+        {
+            letter = "D";
+        }
+        else if (percent <=60 && percent >= 0)
+        {
+            letter = "F";
+        }
+        else 
+        {
+            letter = "the percentage is not valid";
+        }
+
+
+        if(percent>=70)
+        {
+            Console.WriteLine("Pass the class");
+        }
+        else 
+        {
+            Console.WriteLine("try again, you can do it");
+        }
+        
+
+        Console.Write($"Your grade is {letter}");
+
     }
 }
