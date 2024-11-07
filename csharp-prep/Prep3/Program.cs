b4f45e7bcc637d39861780e3cf5a8fa2ba209b87
joshuacirilo <u175692795@byui.edu>
2024-11-07 17:07:59 -0600

    guessnumber

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
+
         Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int MagicNumber = randomGenerator.Next(1, 100);
+        
+
+
+        int GuessNumber = 0;
+
+        do
+        {
+            Console.Write("What is your gess?  ");
+            GuessNumber = int.Parse(Console.ReadLine());
+
+
+            if (MagicNumber > GuessNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (MagicNumber < GuessNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (MagicNumber == GuessNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (MagicNumber != GuessNumber);
+
+
+
+
     }
+
 }
